393689deec9456cc621e1009c9c9ed8ca32013ce
Ebrahim <Ebrahim.elhabashy354@gmail.com>
2023-09-10 04:33:04 +0300

    get is hopfully done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const App = express();
 
-const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { S3Client, GetObjectCommand, HeadObjectCommand, GetObjectTaggingCommand } = require('@aws-sdk/client-s3');
 const s3 = new S3Client();
 //packages
 const morgan = require("morgan");
@@ -37,16 +37,43 @@ App.use("/api/orders", orders);
 
 App.get("*", async (req, res) => {
   let filename = req.path.slice(1);
-  const command = new GetObjectCommand({
+  const params = {
     Bucket: BUCKET,
     Key: filename
-  })
-
+  }
   try {
-    let s3File = await s3.send(command);
-    res.attachment(filename);
-    res.type(s3File.ContentType);
-    res.send(s3File.Body);
+    const headResponse = await s3.send(new HeadObjectCommand(params));
+
+    res.set({
+      "Content-Length": headResponse.ContentLength,
+      "Content-Type": headResponse.ContentType,
+      "ETag": headResponse.ETag,
+    });
+      // Get the object taggings (optional)
+        if (streamTags === true) {
+            const taggingResponse = await s3.send(new GetObjectTaggingCommand(params));
+            taggingResponse.TagSet.forEach((tag) => {
+                res.set("X-TAG-" + tag.Key, tag.Value);
+            });
+        }
+        // Prepare cache headers
+        if (typeof cacheExpiration === "number") {
+            res.setHeader("Cache-Control", "public, max-age=" + cacheExpiration / 1000);
+            res.setHeader("Expires", new Date(Date.now() + cacheExpiration).toUTCString());
+        } else {
+            res.setHeader("Pragma", "no-cache");
+            res.setHeader("Cache-Control", "no-cache");
+            res.setHeader("Expires", 0);
+        }
+   const response = await s3.send(new GetObjectCommand(params));
+        const stream = response.Body;
+        stream.on("data", (chunk) => res.write(chunk));
+        stream.once("end", () => {
+            res.end();
+        });
+        stream.once("error", () => {
+            res.end();
+        });
   } catch (error) {
     if (error.code === "NoSuchKey") {
       console.log(`No such key ${filename}`);
